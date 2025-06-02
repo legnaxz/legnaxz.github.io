21f42532999859f99db667838c2bbc9cd5d5d3d1
legnaxz <legnaxz@naver.com>
2025-06-02 09:22:23 +0000

    update site

---
+++ docs/_app/immutable/entry/start.DPfE0xwh.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BTI8CfSB.js";export{o as load_css,r as start};
--- src/routes/+page.server.ts
+++
@@ -1,5 +0,0 @@
-// +page.server.ts
-export function load() {
-  const adId = 'AD-' + Math.floor(Math.random() * 1000000);
-  return { adId };
-}
