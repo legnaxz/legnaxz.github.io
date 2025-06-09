ed76f17ba5540847561dae28187c17cd93e53766
legnaxz <legnaxz@naver.com>
2025-06-09 05:47:37 +0000

    update

--- src/lib/api/fetchVideo.ts
+++ src/lib/api/fetchVideo.ts
@@ -1,5 +1,5 @@
 export async function fetchFbVideo(adUrl: string) {
-    const apiBase = 'https://6671-118-130-112-221.ngrok-free.app'; // ngrok 주소
+    const apiBase = 'https://aa7b-118-130-112-221.ngrok-free.app'; // ngrok 주소
     const encodedUrl = encodeURIComponent(adUrl);
     const endpoint = `${apiBase}/fb-video?url=${encodedUrl}`;
 
